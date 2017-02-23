fa14dcbdddc7091cfca8c0b256fc25b803993557
Horsetoast <peciar.matus@gmail.com>
2017-02-23 19:35:00 +0800

    Extension config and manifest settings

--- extension/config/config.js
+++ extension/config/config.js
@@ -1,6 +1,6 @@
 var config = {
-  api: 'https://reactions-plus.herokuapp.com',
-  fbProxyUrl: 'https://reactions-plus.herokuapp.com/facebook-login',
-  fbSuccessUrl: 'https://reactions-plus.herokuapp.com/login-success',
+  api: 'https://matus-rpce.herokuapp.com',
+  fbProxyUrl: 'https://matus-rpce.herokuapp.com/facebook-login',
+  fbSuccessUrl: 'https://matus-rpce.herokuapp.com/login-success',
   fbAppId: '1342237909168693'
 }
